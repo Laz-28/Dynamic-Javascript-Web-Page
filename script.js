d90c271d0e064d926c642f0075313eeaaa7f8021
Lazaro <otienolazaro111@gmail.com>
2026-06-19 15:00:29 +0300

    Add delete button next to the listed element

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ function show(event){
     event.preventDefault();
     const listText = theString.value.trim();
 
+    if(listText === '') return;
+
 const li =  document.createElement('li');
 const textSpan = document.createElement('span');
 textSpan.textContent = listText;
 li.appendChild(textSpan);
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Delete';
+deleteBtn.classList.add('delete-btn');
+
+deleteBtn.addEventListener('click', function(){
+    li.remove();
+});
+
+li.appendChild(deleteBtn);
+
 
 display.appendChild(li);
 theString.value = "";
